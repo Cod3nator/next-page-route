0e3d2616e4a65926862e6561d260981da93b5d54
Asish <asishbosek@gmail.com>
2024-02-01 18:56:26 +0530

    Next.js app
    Next.js app with page routing

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,20 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { NotificationProvider } from "../component/NotificationContext";
+import Headers from "../component/Headers";
+import Footer from "../component/Footer";
 
 export default function Document() {
   return (
-    <Html lang="en">
+   <NotificationProvider>
+     <Html lang="en">
       <Head />
       <body>
+        <Headers/>
         <Main />
         <NextScript />
+        <Footer/>
       </body>
     </Html>
+   </NotificationProvider>
   );
 }
---
+++ src/pages/main/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Content from '../../component/Content'
+
+const Main = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Main
